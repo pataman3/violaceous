14df042f9bcd87eea38ea0ca0f294efc9fc1c6e5
Peter Lavigne <peterklavigne@gmail.com>
2017-11-20 20:39:02 -0500

    + basic physics collisions, foliage, camera motion, controls

--- game/scripts/controller.gd
+++ game/scripts/controller.gd
@@ -1,14 +1,18 @@
-extends Sprite
+extends KinematicBody2D
 
 # children of this node
-onready var anim = get_node("anim")
+onready var animator = get_node("animator")
+onready var sprite = get_node("sprite")
 
-# horizontal velocity
-var x_vel = 0
+# gravity is the speed the player falls in pixels/sec
+var GRAVITY = 10;
+
+# velocity
+var velocity = Vector2(0, GRAVITY)
 
 # default walking/running speeds
 export var RUN_SPEED = 7
-export var WALK_SPEED = 3.35
+export var WALK_SPEED = 3.30
 
 # the minimum speed to move
 var WALK_MIN = 1
@@ -26,7 +30,10 @@ func _ready():
 
 # called every frame
 func _process(delta):
-	move_local_x(x_vel)
+	if (is_colliding()):
+        move(get_collision_normal().slide(velocity))
+
+	move(velocity)
 
 
 # called whenever an input is recieved
@@ -46,35 +53,35 @@ func _key_input(event):
 	# call event.scancode to get the key just pressed / released
 	# should probably call event.is_echo() to check for repeat inputs
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
-		x_vel = WALK_SPEED
+		velocity.x = WALK_SPEED
 	elif Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
-		x_vel = -WALK_SPEED
+		velocity.x = -WALK_SPEED
 	else:
-		x_vel = 0
+		velocity.x = 0
 	
-	if Input.is_key_pressed(KEY_SHIFT) and x_vel != 0:
-		x_vel = RUN_SPEED * sign(x_vel)
+	if Input.is_key_pressed(KEY_SHIFT) and velocity.x != 0:
+		velocity.x = RUN_SPEED * sign(velocity.x)
 
 
 # called by _input when a joystick input is recieved
 func _joystick_input(event):
-	x_vel = Input.get_joy_axis(0, JOY_AXIS_0) * RUN_SPEED
-	if abs(x_vel) < WALK_MIN:
-		x_vel = 0
+	velocity.x = Input.get_joy_axis(0, JOY_AXIS_0) * RUN_SPEED
+	if abs(velocity.x) < WALK_MIN:
+		velocity.x = 0
 
 
-# sets the animation based on Ramona's x_vel
+# sets the animation based on Ramona's velocity
 func _set_anim():
-	var current_anim = anim.get_current_animation()
+	var current_anim = animator.get_current_animation()
 	
-	var next_anim = "ramona_run"
-	if -WALK_SPEED <= x_vel and x_vel <= WALK_SPEED:
-		next_anim = "ramona_walk"
-	if x_vel == 0:
-		next_anim = "ramona_idle"
+#	var next_anim = "ramona_run"
+#	if -WALK_SPEED <= x_vel and x_vel <= WALK_SPEED:
+#		next_anim = "ramona_walk"
+#	if x_vel == 0:
+#		next_anim = "ramona_idle"
+#	
+#	if current_anim != next_anim:
+#		animator.play(next_anim)
 	
-	if current_anim != next_anim:
-		anim.play(next_anim)
-	
-	if x_vel != 0:
-		set_flip_h(x_vel < 0)
+	if velocity.x != 0:
+		sprite.set_flip_h(velocity.x < 0)
