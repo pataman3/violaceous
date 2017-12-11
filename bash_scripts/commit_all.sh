b8c3f7df80bbdbadfe5a36b3fdc1c627e5bdd882
Peter Lavigne <peterklavigne@gmail.com>
2017-12-11 14:49:14 -0500

    + added comments to bash scripts

--- bash_scripts/commit_all.sh
+++ bash_scripts/commit_all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# adds every change to git and commits
 git add .
 git commit -m "$1"
--- bash_scripts/run_game.sh
+++ bash_scripts/run_game.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-export PATH=/usr/local/bin:$PATH
-cd ~/programming/godot/violaceous
+# pulls and runs the game. this code is used to create a shortcut
+# to the game that runs the latest version. Kyle uses a modified
+# version of the code (different file path to project folder) in
+# his shortcut.
+export PATH=/usr/local/bin:$PATH # may not work in windows/linux
+cd ~/programming/godot/violaceous # unique to peter's mac
 git pull https://github.com/pataman3/violaceous.git
 cd game
 godot
