6ce7d7a9e96056088a59f0b6952a97216482eec2
Peter Lavigne <peterklavigne@gmail.com>
2017-12-11 13:53:36 -0500

    / refactoring controller variables

--- game/scripts/controller.gd
+++ game/scripts/controller.gd
@@ -4,11 +4,8 @@ extends KinematicBody2D
 onready var animator = get_node("animator")
 onready var sprite = get_node("sprite")
 
-# gravity is the speed the player falls in pixels/sec
-var GRAVITY = 10
-
-# velocity
-var velocity = Vector2(0, GRAVITY)
+# x velocity
+var x_velocity = 0
 
 # default walking/running speeds
 export var RUN_SPEED = 7
@@ -24,16 +21,13 @@ var supported_inputs = [InputEvent.KEY, InputEvent.JOYSTICK_MOTION]
 
 # called on start
 func _ready():
-	set_process_input(true)
-	set_process(true)
+	set_process_input(true) # recieve input events
+	set_process(true)       # run code every frame
 
 
 # called every frame
 func _process(delta):
-	if is_colliding():
-        move(get_collision_normal().slide(velocity))
-
-	move(velocity)
+	move(Vector2(x_velocity, 0))
 
 
 # called whenever an input is recieved
@@ -50,24 +44,26 @@ func _input(event):
 
 # called by _input when a key input is recieved
 func _key_input(event):
+	# ignore repeating keys
+	if event.is_echo():
+		return
 	# call event.scancode to get the key just pressed / released
-	# should probably call event.is_echo() to check for repeat inputs
 	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
-		velocity.x = WALK_SPEED
+		x_velocity = WALK_SPEED
 	elif Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
-		velocity.x = -WALK_SPEED
+		x_velocity = -WALK_SPEED
 	else:
-		velocity.x = 0
+		x_velocity = 0
 	
-	if Input.is_key_pressed(KEY_SHIFT) and velocity.x != 0:
-		velocity.x = RUN_SPEED * sign(velocity.x)
+	if Input.is_key_pressed(KEY_SHIFT) and x_velocity != 0:
+		x_velocity = RUN_SPEED * sign(x_velocity)
 
 
 # called by _input when a joystick input is recieved
 func _joystick_input(event):
-	velocity.x = Input.get_joy_axis(0, JOY_AXIS_0) * RUN_SPEED
-	if abs(velocity.x) < WALK_MIN:
-		velocity.x = 0
+	x_velocity = Input.get_joy_axis(0, JOY_AXIS_0) * RUN_SPEED
+	if abs(x_velocity) < WALK_MIN:
+		x_velocity = 0
 
 
 # sets the animation based on Ramona's velocity
@@ -83,5 +79,5 @@ func _set_anim():
 #	if current_anim != next_anim:
 #		animator.play(next_anim)
 	
-	if velocity.x != 0:
-		sprite.set_flip_h(velocity.x < 0)
+	if x_velocity != 0:
+		sprite.set_flip_h(x_velocity < 0)
