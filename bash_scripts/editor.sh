367a728705540cadde3941c13d71ba36d284514d
Peter Lavigne <peterklavigne@gmail.com>
2018-05-23 21:17:34 -0400

    Clean everything up and start fresh

--- bash_scripts/editor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# opens the project in the Godot editor
-cd ../game
-godot -e > /dev/null & # supress standard output
--- bash_scripts/reset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# completely wipes any changes
-# resets the repo to the newest version
-git reset --hard origin
-git clean -f -d
-git pull
