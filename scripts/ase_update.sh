c98d2a0332284314b573a10bdbfb71413c041b1f
pataman3 <bryanbean@protonmail.ch>
2018-05-30 11:17:29 -0400

    Fixed even, even more icky grammar

--- scripts/ase_update.sh
+++ scripts/ase_update.sh
@@ -7,18 +7,18 @@ USER=$(<user.txt)
 
 # TODO(bryan): Using jq and eval makes for a bit of a nasty solution. There's
 #   got to be a better way around this.
-SOURCE="jq -r '.$USER.ase_dir' collaborators.json"
-SOURCE=$(eval "$SOURCE")
+SOURCE="jq -r '.${USER}.ase_dir' collaborators.json"
+SOURCE=$(eval "${SOURCE}")
 
-VIO_DIR="jq -r '.$USER.vio_dir' collaborators.json"
-VIO_DIR=$(eval "$VIO_DIR")
-DESTINATION=$VIO_DIR
-DESTINATION+="/assets/sprites/$USER/"
+VIO_DIR="jq -r '.${USER}.vio_dir' collaborators.json"
+VIO_DIR=$(eval "${VIO_DIR}")
+DESTINATION=${VIO_DIR}
+DESTINATION+="/assets/sprites/${USER}/"
 
-cd "$SOURCE" || exit
-find . -iname '*.ase' -exec cp -r {} "$DESTINATION" \;
+cd "${SOURCE}" || exit
+find . -iname '*.ase' -exec cp -r {} "${DESTINATION}" \;
 
-cd "$VIO_DIR" || exit
-git add assets/sprites/"$USER"/
-git commit -m "Changed $USER's assets"
+cd "${VIO_DIR}" || exit
+git add assets/sprites/"${USER}"/
+git commit -m "Changed ${USER}'s assets"
 git push origin master
--- scripts/run_game.sh
+++ scripts/run_game.sh
@@ -7,13 +7,13 @@ USER=$(<user.txt)
 
 # TODO(bryan): Using jq and eval makes for a bit of a nasty solution. There's
 #   got to be a better way around this.
-VIO_DIR="jq -r '.$USER.vio_dir' collaborators.json"
-VIO_DIR=$(eval "$VIO_DIR")
+VIO_DIR="jq -r '.${USER}.vio_dir' collaborators.json"
+VIO_DIR=$(eval "${VIO_DIR}")
 
-cd "$VIO_DIR" || exit
+cd "${VIO_DIR}" || exit
 git pull
 
 cd game || exit
-GOD_BIN="jq -r '.$USER.god_bin' $VIO_DIR/scripts/collaborators.json"
-GOD_BIN=$(eval "$GOD_BIN")
-( $GOD_BIN )
+GOD_BIN="jq -r '.${USER}.god_bin' ${VIO_DIR}/scripts/collaborators.json"
+GOD_BIN=$(eval "${GOD_BIN}")
+( ${GOD_BIN} )
